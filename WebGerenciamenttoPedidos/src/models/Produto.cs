9020d5d434be19626d4c524836ce6a1a8b70ed7d
Gabriel Silva <gabriel.silva@moveresoftware.com>
2024-05-20 13:15:57 -0400

    add 4 chnages

---
+++ WebGerenciamenttoPedidos/src/models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebGerenciamenttoPedidos.src.models
+{
+	public class Produto
+	{
+		public int id_produto { get; set; }
+		public string descricao { get; set; }
+		
+
+	}
+}
