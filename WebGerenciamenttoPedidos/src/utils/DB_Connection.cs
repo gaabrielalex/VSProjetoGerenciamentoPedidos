72f6db80a04e7539665342cff74716bc74b23243
Gabriel Silva <gabriel.silva@moveresoftware.com>
2024-05-20 14:14:30 -0400

    add DB_Connection class

---
+++ WebGerenciamenttoPedidos/src/utils/DB_Connection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebGerenciamenttoPedidos.src.utils
+{
+	public class DB_Connection
+	{
+		public static SqlConnection getConnection() {
+			string connectionString = ConfigurationManager.ConnectionStrings["WebGerenciamenttoPedidos.Properties.Settings.db_connectiono"].ConnectionString;
+			return new SqlConnection(connectionString);
+		}
+
+	}
+}
