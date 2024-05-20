84649fce282e0c11c87535bf374e6104e45e75b4
Gabriel Silva <gabriel.silva@moveresoftware.com>
2024-05-20 13:31:41 -0400

    add produto model

--- WebGerenciamenttoPedidos/src/models/Produto.cs
+++ WebGerenciamenttoPedidos/src/models/Produto.cs
@@ -7,9 +7,15 @@ namespace WebGerenciamenttoPedidos.src.models
 {
 	public class Produto
 	{
-		public int id_produto { get; set; }
-		public string descricao { get; set; }
+		public int IdPproduto { get; set; }
+		public String Descricao { get; set; }
 		
+		public decimal VlrUnitario { get; set; }
 
+		public Produto(int idProduto, String descricao, decimal vlrUnitario) {
+			IdPproduto = idProduto;
+			Descricao = descricao;
+			VlrUnitario = vlrUnitario;
+		}
 	}
 }
