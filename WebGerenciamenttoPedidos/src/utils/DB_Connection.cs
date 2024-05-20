1f8b849b17a4facded8a1b17e54ea6dae39f747a
Gabriel Silva <gabriel.silva@moveresoftware.com>
2024-05-20 15:02:21 -0400

    correção conexão com banco

--- WebGerenciamenttoPedidos/src/utils/DB_Connection.cs
+++ WebGerenciamenttoPedidos/src/utils/DB_Connection.cs
@@ -10,8 +10,12 @@ namespace WebGerenciamenttoPedidos.src.utils
 	public class DB_Connection
 	{
 		public static SqlConnection getConnection() {
-			string connectionString = ConfigurationManager.ConnectionStrings["WebGerenciamenttoPedidos.Properties.Settings.db_connectiono"].ConnectionString;
-			return new SqlConnection(connectionString);
+			//Assim tava dadno erro
+			//string connectionString = ConfigurationManager.ConnectionStrings["WebGerenciamenttoPedidos.Properties.Settings.db_connection"].ConnectionString;
+			//return new SqlConnection(connectionString);
+
+			//Corrigido, peguei a string direto nas propriedades do projeto
+			return new SqlConnection("Data Source=GABRIELSILVA\\SQLEXPRESS;Initial Catalog=DB_Gerenciamento_Pedidos;Integrated Security=True;Encrypt=False;TrustServerCertificate=True");
 		}
 
 	}
