8520b11ab65f81e903537748efbbce2735ecf54d
Gabriel Silva <gabriel.silva@moveresoftware.com>
2024-05-22 17:51:46 -0400

    .

---
+++ TestesGerenciamentoPedidos/UtilTeste.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data.SqlClient;
+using WebGereciamentoPedidos.src.util;
+
+namespace TestesGerenciamentoPedidos
+{
+	[TestClass]
+	public class UtilTeste
+	{
+		private readonly string[] TABELAS_BD = { "item_pedido", "pedido", "produto", "metodo_pagto" };
+
+		[TestMethod]
+		public void ApagarTodosOsRegristosTesteDoBanco()
+		{
+			try
+			{
+				foreach(string tabela in TABELAS_BD)
+				{
+					using (SqlConnection con = DB_Connection.getConnection())
+					{
+						string sql = "DELETE FROM produto WHERE descricao LIKE '%Teste%'";
+						con.Open();
+						SqlCommand cmd = new SqlCommand(sql, con);
+						cmd.ExecuteNonQuery();
+						con.Close();
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				e.Should().BeNull(because: "Não deve ocorrer erro ao apagar registros de teste do banco");
+			}
+		}
+	}
+}
