3dbc472172e3dc941cb169024070a66fd30fb016
Gabriel Silva <gabriel.silva@moveresoftware.com>
2024-05-27 11:48:33 -0400

    add funcionalidade de contar a quantidade de digitos de um numero antes do separador de cimal

---
+++ TestesGerenciamentoPedidos/ModelUtilsTestecs.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WebGereciamentoPedidos.src.util;
+
+namespace TestesGerenciamentoPedidos
+{
+	[TestClass]
+	public class ModelUtilsTestecs
+	{
+		[TestMethod]
+		public void AoObterQuantidadeDeDigitosAntesDoSeparadorDecimalDeUmValorComDuasCasasDecimaisDeveRetornarDois()
+		{
+			// Arrange
+			decimal valor = 123.45m;
+			// Act
+			int quantidadeDigitos = ModelUtils.ObterQuantidadeDeDigitosAntesDoSeparadorDecimal(valor);
+			// Assert
+			int quantidadeDigitosEsperada = 3;
+			quantidadeDigitos.Should().Be(quantidadeDigitosEsperada, because: "O valor informado possui 3 digitos antes do separador decimal");
+		}
+		
+	}
+}
---
+++ WebGereciamentoPedidos/src/util/ModelUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebGereciamentoPedidos.src.util
+{
+	public class ModelUtils
+	{
+		public static int ObterQuantidadeDeDigitosAntesDoSeparadorDecimal(decimal valor)
+		{
+			var quantidadeDigitosAntesDoSeparadorDecimal = 0;
+			string valorString = valor.ToString();
+			if(valorString.Contains("."))
+			{
+				quantidadeDigitosAntesDoSeparadorDecimal = valorString.Split('.')[0].Length;
+			}
+			else if (valorString.Contains(","))
+			{
+				quantidadeDigitosAntesDoSeparadorDecimal = valorString.Split(',')[0].Length;
+			}
+			else
+			{
+				quantidadeDigitosAntesDoSeparadorDecimal = valorString.Length;
+			}
+			return quantidadeDigitosAntesDoSeparadorDecimal;
+			
+		}
+	}
+}
