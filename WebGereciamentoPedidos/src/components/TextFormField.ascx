9ef77148735ddafdf6835bef3f50ff54bbe2e685
Gabriel Silva <gabriel.silva@moveresoftware.com>
2024-05-30 10:37:39 -0400

    implementação do componente text form field

---
+++ WebGereciamentoPedidos/src/components/TextFormField.ascx
@@ -0,0 +1,30 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="TextFormField.ascx.cs" Inherits="WebGereciamentoPedidos.src.components.TextFormField" %>
+
+<style>
+    .erro {
+	    color: #dc3545;
+    }
+
+    .gp-text-box {
+        display: inline-flex;
+        flex-direction: column;
+        margin-top: 6px;
+        max-width: 280px;
+    }
+
+    .gp-text-form-field {
+        display: inline-flex;
+        flex-direction: column;
+    }
+</style>
+
+<div class="gp-text-form-field">
+    <asp:Label ID="Label" runat="server"></asp:Label>
+    <div class="gp-text-box <%=CssClass%>">
+	    <asp:TextBox ID="TextBox" runat="server"></asp:TextBox>
+        <asp:CustomValidator ID="CustomValidator" runat="server" ControlToValidate="TextBox" ErrorMessage=""
+        CssClass="erro" OnServerValidate="CustomValidator_ServerValidate" ValidateEmptyText="true"></asp:CustomValidator>
+        <%--ValidateEmptyText="true" serve para que o validation seja chamado mesmo que o campo seja nulo, desta pode ser feito 
+        também validação de campo obrigatório no lado do servidor sem usar o require requireValidaion--%>
+    </div>
+</div>
---
+++ WebGereciamentoPedidos/src/components/TextFormField.ascx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebGereciamentoPedidos.src.components
+{
+	public partial class TextFormField : System.Web.UI.UserControl
+	{
+		public string CssClass { get; set; }
+		public Label LabelControl
+		{
+			get
+			{
+				return this.Label;
+			}
+		}
+		public TextBox TextBoxControl
+		{
+			get 
+			{
+				return this.TextBox;
+			}
+			set
+			{
+				this.TextBox = value;
+			}
+		}
+		public CustomValidator CustomValidatorControl
+		{
+			get
+			{
+				return this.CustomValidator;
+			}
+			set
+			{
+				this.CustomValidator = value;
+			}
+		}
+		public string Text
+		{
+			get
+			{
+				return this.TextBox.Text;
+			}
+			set
+			{
+				this.TextBox.Text = value;
+			}
+		}
+		public string LabelText
+		{
+			get
+			{
+				return this.Label.Text;
+			}
+			set
+			{
+				this.Label.Text = value;
+			}
+		}
+		public string ValidationGroup
+		{
+			get
+			{
+				return this.CustomValidator.ValidationGroup;
+			}
+			set
+			{
+				this.CustomValidator.ValidationGroup = value;	
+			}
+		}
+
+		public string ErrorMessage
+		{
+			get
+			{
+				return this.CustomValidator.ErrorMessage;
+			}
+			set
+			{
+				this.CustomValidator.ErrorMessage = value;
+			}
+		}
+
+		public event ServerValidateEventHandler ServerValidate;
+		protected void CustomValidator_ServerValidate(object source, ServerValidateEventArgs args)
+		{
+			ServerValidate?.Invoke(source, args);
+		}
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+		}
+	}
+}
