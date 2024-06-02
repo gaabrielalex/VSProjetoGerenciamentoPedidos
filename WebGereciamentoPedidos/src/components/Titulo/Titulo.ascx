801633446db6f0dba935278afb368bd862e89a6e
Gabriel Silva <gabriel.silva@moveresoftware.com>
2024-06-02 13:54:51 -0400

    add componente titulo

---
+++ WebGereciamentoPedidos/src/components/Titulo/Titulo.ascx
@@ -0,0 +1,12 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Titulo.ascx.cs" Inherits="WebGereciamentoPedidos.src.components.Titulo.Titulo" %>
+<style>
+	.title-container {
+		margin: 40px 0;
+	}
+</style>
+
+<div class="title-container">
+	<h1 runat="server">
+		<asp:Label ID="TituloLabel" runat="server" Text="Título" ></asp:Label>
+	</h1>
+</div>
---
+++ WebGereciamentoPedidos/src/components/Titulo/Titulo.ascx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebGereciamentoPedidos.src.components.Titulo
+{
+	public partial class Titulo : System.Web.UI.UserControl
+	{
+		public string Text
+		{
+			get
+			{
+				return TituloLabel.Text;
+			}
+			set 
+			{ 
+				TituloLabel.Text = value;
+			}
+		}
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
