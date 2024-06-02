f4ab08d6d6f45f05a550d170dd6946c08fbb5bc8
Gabriel Silva <gabriel.silva@moveresoftware.com>
2024-06-02 13:22:15 -0400

    .

---
+++ WebGereciamentoPedidos/src/util/ModosFomularios.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebGereciamentoPedidos.src.util
+{
+	public enum ModosFomularios
+	{
+		Cadastrar,
+		Editar
+
+	}
+}
