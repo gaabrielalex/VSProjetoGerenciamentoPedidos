b43f979ed5262e0f5d053871949ce193243ecae8
Gabriel Silva <gabriel.silva@moveresoftware.com>
2024-06-02 15:36:05 -0400

    definindo layout do formula´riuo produto

---
+++ WebGereciamentoPedidos/src/pages/ProdutoPages/FormAddEditProduto/FormAddEditProduto.ascx
@@ -0,0 +1,50 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="FormAddEditProduto.ascx.cs" Inherits="WebGereciamentoPedidos.src.pages.ProdutoPages.FormAddEditProduto.FormAddEditProduto" %>
+<%@ Register TagPrefix="gp" TagName="TextFormField" Src="~/src/components/TextFormField/TextFormField.ascx" %>
+<%@ Register TagPrefix="gp" TagName="Titulo" Src="~/src/components/Titulo/Titulo.ascx" %>
+
+<style>
+	.conteudo-FormAddEditProdutoPanel .row {
+		margin-bottom: 20px;
+	}
+
+		.conteudo-FormAddEditProdutoPanel .row:last-child {
+			margin-top: 30px;
+		}
+	.buttons-FormAddEditProdutoPanel {
+		display: flex;
+		justify-content: end;
+		gap: 15px;
+	}
+
+	.conteudo-FormAddEditProdutoPanel {
+		width: 400px;
+	}
+
+</style>
+
+<asp:UpdatePanel runat="server">
+	<ContentTemplate>
+		<asp:Panel runat="server" ID="FormAddEditProdutoPanel" Visible="false">
+			<gp:Titulo runat="server" Text="Cadastrar Produto" ID="FormAddEditProdutoTitulo"></gp:Titulo>
+			<div class="conteudo-FormAddEditProdutoPanel">
+				<div class="row">
+					<gp:TextFormField runat="server" ID="DescricaoTextFormField" LabelText="Descrição" Style="margin-right: 20px"
+						ValidationGroup="CamposProduto" OnServerValidate="DescricaoTextFormField_ServerValidate" />
+				</div>
+				<div class="row d-flex justify-content-end"">
+					<gp:TextFormField runat="server" ID="VlrUnitarioTextFormField" LabelText="Valor Unitário" Format="dinheiro"
+						ValidationGroup="CamposProduto" OnServerValidate="VlrUnitarioTextFormField_ServerValidate" />
+				</div>
+				<div class="row">
+					<div class="buttons-FormAddEditProdutoPanel">
+						<asp:Button runat="server" ID="Button1" ValidationGroup="CamposProduto" Text="Cancelar"
+							CssClass="btn btn-lg btn-secondary SubmitButtonModalFormAddEditProduto" CausesValidation="false" />
+						<asp:Button runat="server" ID="Button2" ValidationGroup="CamposProduto" Text="Salvar"
+							CssClass="btn btn-lg btn-primary SubmitButtonModalFormAddEditProduto" />
+					</div>
+				</div>
+			</div>
+		</asp:Panel>
+	</ContentTemplate>
+</asp:UpdatePanel>
+
---
+++ WebGereciamentoPedidos/src/pages/ProdutoPages/FormAddEditProduto/FormAddEditProduto.ascx.cs
@@ -0,0 +1,110 @@
+﻿using DAOGerenciamentoPedidos.Src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UtilsGerenciamentoPedidos;
+using WebGereciamentoPedidos.src.util;
+
+namespace WebGereciamentoPedidos.src.pages.ProdutoPages.FormAddEditProduto
+{
+	public partial class FormAddEditProduto : System.Web.UI.UserControl
+	{
+		public ProdutoDAO ProdutoDAO;
+		public ModosFomularios ModoAtual
+		{
+			get
+			{ 
+				if(ViewState["ModoAtual"] != null) {
+					return (ModosFomularios)ViewState["ModoAtual"];
+				}
+				return ModosFomularios.Cadastrar;
+			}
+			set 
+			{
+				ViewState["ModoAtual"] = value;
+			}
+		}
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			ProdutoDAO = new ProdutoDAO();
+		}
+
+		public void abrirForm(ModosFomularios modo) 
+		{
+			FormAddEditProdutoPanel.Visible = true;
+			ModoAtual = modo;
+		}
+
+		protected void DescricaoTextFormField_ServerValidate(object source, ServerValidateEventArgs args)
+		{
+			string descricao = args.Value;
+
+			//Validação se campo obrigatório
+			if (descricao == "")
+			{
+				DescricaoTextFormField.ErrorMessage = "Campo obrigatório!";
+				args.IsValid = false;
+			}
+
+			//Validação de tamanho limite da string
+			if (descricao.Length > 200)
+			{
+				DescricaoTextFormField.ErrorMessage = "Tamanho máximo de 200 caracteres excedido!";
+				args.IsValid = false;
+			}
+
+			//Validação da já existência do produto
+			bool produtoJaExiste = false;
+			try
+			{
+				produtoJaExiste = ProdutoDAO.DescricaoJaExiste(descricao);
+				if (produtoJaExiste)
+				{
+					DescricaoTextFormField.ErrorMessage = "Produto já existente!";
+					args.IsValid = false;
+				}
+			}
+			catch (Exception ex)
+			{
+				PageUtils.MostrarMensagemViaToast("Houve um erro ao validar campo descrição", "e", Page);
+				RegistroLog.Log($"Erro ao validar campo descrição: {ex.ToString()}");
+			}
+		}
+		protected void VlrUnitarioTextFormField_ServerValidate(object source, ServerValidateEventArgs args)
+		{
+			string vlrUnitario = args.Value;
+
+			//Continua para mim copilot
+			//Validação se campo obrigatório
+			if (vlrUnitario == "")
+			{
+				VlrUnitarioTextFormField.ErrorMessage = "Campo obrigatório!";
+				args.IsValid = false;
+				return;
+			}
+
+			//Validação de valor numérico
+			if (!decimal.TryParse(vlrUnitario, out decimal vlrUnitarioDecimal))
+			{
+				VlrUnitarioTextFormField.ErrorMessage = "Valor inválido!";
+				args.IsValid = false;
+			}
+
+			//Validação de valor máximo de dígitos
+			string digitosString = vlrUnitarioDecimal.ToString();
+			if (!digitosString.Contains(','))
+			{
+				digitosString += ",0";
+			}
+			string[] digitos = (digitosString).Split(',');
+			if (digitos[0].Length > 6 || digitos[1].Length > 2)
+			{
+				VlrUnitarioTextFormField.ErrorMessage = "Valor deve ter no máximo 2 casas decimais e 6 dígitos!";
+				args.IsValid = false;
+			}
+		}
+	}
+}
