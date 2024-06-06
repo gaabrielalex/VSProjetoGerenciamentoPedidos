9bc85e47e527922f22352ad49a671f3b04d7be98
Gabriel Silva <gabriel.silva@moveresoftware.com>
2024-06-06 11:59:16 -0400

    add model Metdo ´pagamento

---
+++ ModelsGerenciamentoPedidos/Src/MetodoPagamento.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsGerenciamentoPedidos.Src
+{
+	public  class MetodoPagamento
+	{
+		public int IdMetodoPagto { get; set; }
+		public string Descricao { get; set; }
+
+		public MetodoPagamento()
+		{
+		}
+
+		public MetodoPagamento(int idMetodoPagto, string descricao)
+		{
+			IdMetodoPagto = idMetodoPagto;
+			Descricao = descricao;
+		}
+
+		public override bool Equals(object obj)
+		{
+			return obj is MetodoPagamento pagamento &&
+				   IdMetodoPagto == pagamento.IdMetodoPagto &&
+				   Descricao == pagamento.Descricao;
+		}
+
+		public override int GetHashCode()
+		{
+			int hashCode = 1308185697;
+			hashCode = hashCode * -1521134295 + IdMetodoPagto.GetHashCode();
+			hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Descricao);
+			return hashCode;
+		}
+	}
+}
