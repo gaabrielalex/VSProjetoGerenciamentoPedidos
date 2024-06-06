0824661a20fe92c3ffea957f063aea195673bd21
Gabriel Silva <gabriel.silva@moveresoftware.com>
2024-06-06 11:04:24 -0400

    implemetacao da interface IDAO

---
+++ DAOGerenciamentoPedidos/Src/IDAO.cs
@@ -0,0 +1,22 @@
+﻿using ModelsGerenciamentoPedidos.Src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAOGerenciamentoPedidos.Src
+{
+	public interface IDAO<T> where T : class
+	{
+		int Inserir(T obj);
+
+		void Editar(T obj, int idObjASerEditado);
+
+		void Excluir(int id);
+
+		List<T> Listar();
+
+		T ObterPorId(int id);
+	}
+}
