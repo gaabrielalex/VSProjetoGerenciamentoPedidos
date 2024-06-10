ab992b00f4a8f169836b3f43dbe9ebe84d9a3e1b
Gabriel Silva <gabriel.silva@moveresoftware.com>
2024-06-10 14:11:36 -0400

    implementacaao de classe erro para realizar uma tratameto pardrão no projet

---
+++ UtilsGerenciamentoPedidos/Erro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilsGerenciamentoPedidos
+{
+	public class Erro : Exception
+	{
+		public Erro(string mensagem) : base(mensagem)
+		{
+			RegistroLog.Log(mensagem);
+		}
+	}
+}
