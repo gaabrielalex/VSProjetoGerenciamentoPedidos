ef61ccf54e2964108aaaa0b18f99d5afcc41ba5f
Gabriel Silva <gabriel.silva@moveresoftware.com>
2024-06-24 20:20:14 -0400

    implementando o model do item_pedido

---
+++ ModelsGerenciamentoPedidos/Src/ItemPedido.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsGerenciamentoPedidos.Src
+{
+	public class ItemPedido
+	{
+		public int? IdItemPedido { get; set; }
+		public int IdPedido { get; set; }
+		public Produto Produto { get; set; }
+		public int Quantidade { get; set; }
+		public decimal VlrTotalItem { get; set; }
+	}
+}
