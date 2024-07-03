68a6ab21ce9cbfa4649d95fae50bd200f62a86d2
Gabriel Silva <gabriel.silva@moveresoftware.com>
2024-07-03 17:19:06 -0400

    implenetação pesquisa em look up do produto

---
+++ WebGereciamentoPedidos/src/pages/ProdutoPages/PesquisaLookUpProdutos/PesquisaLookUpProdutos.js
@@ -0,0 +1,14 @@
+﻿
+var myApp = myApp || {};
+
+(function () {
+	myApp.PesquisaLookUpProdutos = {
+		abrirModalPesquisa: function () {
+			$("#modalPesquisaLookUpProdutos").modal("show");
+		},
+		fecharModalPesquisa: function () {
+			$("#modalPesquisaLookUpProdutos").modal("hide");
+		}
+	};
+})();
+
