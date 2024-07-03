1bb39a74d14ffaf81f120d6ce7ed0c3e8ee5ffe8
Gabriel Silva <gabriel.silva@moveresoftware.com>
2024-07-03 17:20:30 -0400

    adição do serviço de mensagem

---
+++ WebGereciamentoPedidos/Scripts/ScritpsProjeto/servico-mensagens-ao-carregar-paginas.js
@@ -0,0 +1,30 @@
+﻿
+var myApp = myApp || {};
+
+(function () {
+	myApp.ServicoMensagensAoCarregarPaginas = {
+		adicionarMensagem: function (messagem, tipo) {
+			localStorage.setItem('menssagemParaSerExibida', JSON.stringify({
+				mensagem: messagem,
+				tipo: tipo
+			}));
+		},
+
+		exibirMensagem: function () {
+			var mensagem = localStorage.getItem('menssagemParaSerExibida');
+
+			if (mensagem) {
+				var mensagemObj = JSON.parse(mensagem);
+				myApp.showToast(mensagemObj.mensagem, mensagemObj.tipo);
+				localStorage.removeItem('menssagemParaSerExibida');
+			}
+		}
+	};
+
+	// Aguarda até que todos os recursos sejam carregados
+	window.onload = function () {
+		// Após a página ser completamente carregada, exibe a mensagem
+		myApp.ServicoMensagensAoCarregarPaginas.exibirMensagem();
+	};
+})();
+
